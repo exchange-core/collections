9c8fc55ef1f828814ee4b7904aa7a98f65d10c9f
Maksim Zheravin <mzheravin@core2.exchange>
2023-03-04 15:06:35 -0500

    random datasets async provider

---
+++ src/test/java/exchange/core2/collections/hashtable/RandomDataSetsProvider.java
@@ -0,0 +1,63 @@
+package exchange.core2.collections.hashtable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class RandomDataSetsProvider {
+
+    private static final Logger log = LoggerFactory.getLogger(HashingUtils.class);
+
+    final int size = 10_000_000;
+    final Random random = new Random(1L);
+
+    private BlockingQueue<long[]> queue = new LinkedBlockingQueue<>(2);
+
+    public static RandomDataSetsProvider create() {
+        final RandomDataSetsProvider provider = new RandomDataSetsProvider();
+        final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+        executor
+            .scheduleWithFixedDelay(provider::generate, 1, 1, TimeUnit.MILLISECONDS);
+        return provider;
+    }
+
+    private RandomDataSetsProvider() {
+
+    }
+
+    public void stop() {
+        //TODO
+    }
+
+    private void generate() {
+
+//        log.debug("Generating dataset, size = {}", size);
+        final long[] data = new long[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = random.nextLong();
+        }
+
+        try {
+//            log.debug("Dataset created, inserting into the queue...");
+            queue.put(data);
+//            log.debug("Dataset inserted");
+        } catch (InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public long[] next() {
+        try {
+//            log.debug("Waiting dataset...");
+            final long[] dataset = queue.take();
+//            log.debug("Dataset taken");
+            return dataset;
+        } catch (InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+
+}
